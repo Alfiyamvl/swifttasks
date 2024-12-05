996de206c94b417865f7b1461f9cd09f726ccac3
Alfiya <35442550+Alfiyamvl@users.noreply.github.com>
2024-12-05 14:08:32 +0500

    Create firstTask.swift
    Конвертация температуры

---
+++ firstTask.swift
@@ -0,0 +1,14 @@
+/* Задание:
+ 1.    Хранит значение температуры в градусах Цельсия в переменной типа Double.
+ 2.    Конвертирует температуру в градусы Фаренгейта по формуле на картинке.
+ F = C * (9/5) + 32
+ 3.    Выводит результат в формате:
+ "Температура: 25.0°C = 77.0°F"
+ */
+
+import UIKit
+
+
+let celsiusTemperature = 25.0
+let fahrenheitTemperature = celsiusTemperature * 1.8 + 32
+print("Температура:", celsiusTemperature, "°C =", fahrenheitTemperature, "°F")

1d014d6f47bfcee9f9859a13ecab80e3e62fc36d
Alfiya <35442550+Alfiyamvl@users.noreply.github.com>
2024-12-09 20:00:23 +0500

    Update and rename eighth.swift to  AreaOfCircle.swift

--- AreaOfCircle.swift
+++ AreaOfCircle.swift
@@ -8,3 +8,11 @@
 3.    Выведи результат в формате:
 "Площадь круга с радиусом 2.0 равна 12.566"
 */
+
+//https://ravesli.com/functions-swift/
+
+import Foundation
+var radius : Double = 2.0
+var powerRadius = pow(radius,2)
+var square = Double.pi * powerRadius
+print("Площадь круга с радиусом \(radius) равна \(square)")

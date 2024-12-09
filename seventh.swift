c6644ce1a657ab7e1aaf3cedd9a66dcf883d540c
Alfiya <35442550+Alfiyamvl@users.noreply.github.com>
2024-12-09 19:40:50 +0500

    Update seventh.swift

--- seventh.swift
+++ seventh.swift
@@ -5,3 +5,24 @@
 2.    Выведи приветствие в формате:
 "Привет, меня зовут Костя, мне 33 года, и я живу в Санкт-Петербурге."
 */
+
+// https://medium.com/@Lakshmnaidu/readline-in-swift-87c27ff3a098
+print("Введите имя:")
+guard var name = readLine() 
+else {
+fatalError("Bad input")
+}
+// Не понятно пока что делать с пустым значением возраста
+print("Введите возраст:")
+guard var ageS = readLine() , var age = Int(ageS)  
+else { 
+fatalError("Bad input") 
+}
+
+print("Введите город, в котором живёте:")
+guard var city = readLine()
+else { 
+fatalError("Bad input") 
+}
+
+print("Привет, меня зовут \(name), мне \(age) года, и я живу в городе \(city)")
